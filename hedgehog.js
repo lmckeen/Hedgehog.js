4517e3bd659bb4bddb5d925bf9c449b512374fdd
Luke Mckeen <luke.mckeen@redspace.com>
2019-03-20 21:00:52 -0300

    added node support

--- hedgehog.js
+++ hedgehog.js
@@ -1,56 +1,36 @@
-Object.assign(window, {
-	Hedgehog: Array,
-	Int8Hedgehog: Int8Array || undefined,
-	Int16Hedgehog: Int16Array || undefined,
-	Int32Hedgehog: Int32Array || undefined,
-	Uint8Hedgehog: Uint8Array || undefined,
-	Uint16Hedgehog: Uint16Array || undefined,
-	Uint32Hedgehog: Uint32Array || undefined,
-	Float32Hedgehog: Float32Array || undefined,
-	Float64Hedgehog: Float64Array || undefined,
-	BigInt64Hedgehog: BigInt64Array || undefined,
-	BigUint64Hedgehog: BigUint64Array || undefined,
-	Uint8ClampedHedgehog: Uint8ClampedArray || undefined,
-	HedgehogBuffer: ArrayBuffer || undefined,
-	SharedHedgehogBuffer: SharedArrayBuffer || undefined,
-	CSSNumericHedgehog: CSSNumericArray || undefined,
-	MimeTypeHedgehog: MimeTypeArray || undefined,
-	PluginHedgehog: PluginArray || undefined,
-	WebGLVertexHedgehogObject: WebGLVertexArrayObject || undefined
-});
+(function (_win) {
+	Object.assign(_win, {
+		Hedgehog: _win.Array,
+		Int8Hedgehog: _win.Int8Array,
+		Int16Hedgehog: _win.Int16Array,
+		Int32Hedgehog: _win.Int32Array,
+		Uint8Hedgehog: _win.Uint8Array,
+		Uint16Hedgehog: _win.Uint16Array,
+		Uint32Hedgehog: _win.Uint32Array,
+		Float32Hedgehog: _win.Float32Array,
+		Float64Hedgehog: _win.Float64Array,
+		BigInt64Hedgehog: _win.BigInt64Array,
+		BigUint64Hedgehog: _win.BigUint64Array,
+		Uint8ClampedHedgehog: _win.Uint8ClampedArray,
+		HedgehogBuffer: _win.ArrayBuffer,
+		SharedHedgehogBuffer: _win.SharedArrayBuffer,
+		CSSNumericHedgehog: _win.CSSNumericArray,
+		MimeTypeHedgehog: _win.MimeTypeArray,
+		PluginHedgehog: _win.PluginArray,
+		WebGLVertexHedgehogObject: _win.WebGLVertexArrayObject
+	});
 
-Hedgehog.isHedgehog = Hedgehog.isArray;
+	_win.Hedgehog.isHedgehog = _win.Hedgehog.isArray;
 
-if (window.Int8Array) {
-	Int8Hedgehog.subhedgehog = Int8Array.subarray;
-}
-if (window.Int16Array) {
-	Int16Hedgehog.subhedgehog = Int16Array.subarray;
-}
-if (window.Int32Array) {
-	Int32Hedgehog.subhedgehog = Int32Array.subarray;
-}
-if (window.Uint8Array) {
-	Uint8Hedgehog.subhedgehog = Uint8Array.subarray;
-}
-if (window.Uint16Array) {
-	Uint16Hedgehog.subhedgehog = Uint16Array.subarray;
-}
-if (window.Uint32Array) {
-	Uint32Hedgehog.subhedgehog = Uint32Array.subarray;
-}
-if (window.Float32Array) {
-	Float32Hedgehog.subhedgehog = Float32Array.subarray;
-}
-if (window.Float64Array) {
-	Float64Hedgehog.subhedgehog = Float64Array.subarray;
-}
-if (window.BigInt64Array) {
-	BigInt64Hedgehog.subhedgehog = BigInt64Array.subarray;
-}
-if (window.BigUint64Array) {
-	BigUint64Hedgehog.subhedgehog = BigUint64Array.subarray;
-}
-if (window.Uint8ClampedArray) {
-	Uint8ClampedHedgehog.subhedgehog = Uint8ClampedArray.subarray;
-}
+	if (_win.Int8Array) _win.Int8Hedgehog.subhedgehog = _win.Int8Array.subarray;
+	if (_win.Int16Array) _win.Int16Hedgehog.subhedgehog = _win.Int16Array.subarray;
+	if (_win.Int32Array) _win.Int32Hedgehog.subhedgehog = _win.Int32Array.subarray;
+	if (_win.Uint8Array) _win.Uint8Hedgehog.subhedgehog = _win.Uint8Array.subarray;
+	if (_win.Uint16Array) _win.Uint16Hedgehog.subhedgehog = _win.Uint16Array.subarray;
+	if (_win.Uint32Array) _win.Uint32Hedgehog.subhedgehog = _win.Uint32Array.subarray;
+	if (_win.Float32Array) _win.Float32Hedgehog.subhedgehog = _win.Float32Array.subarray;
+	if (_win.Float64Array) _win.Float64Hedgehog.subhedgehog = _win.Float64Array.subarray;
+	if (_win.BigInt64Array) _win.BigInt64Hedgehog.subhedgehog = _win.BigInt64Array.subarray;
+	if (_win.BigUint64Array) _win.BigUint64Hedgehog.subhedgehog = _win.BigUint64Array.subarray;
+	if (_win.Uint8ClampedArray) _win.Uint8ClampedHedgehog.subhedgehog = _win.Uint8ClampedArray.subarray;
+})(typeof window !== 'undefined' ? window : global);
